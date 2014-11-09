c0a24fa1fee633f39b9331bd667ba4b319fd185f
Michael Hiland <g2c9@ugrad.cs.ubc.ca>
2014-11-09 08:47:39 -0800

    score box

---
+++ Assets/ScoreBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreBox : MonoBehaviour {
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+		text = this.GetComponent<Text>(); //get the text component in the gameobject you assigned
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = string.Format("Score: {0} {1} ", PlayerScoreKeeper.Score1, PlayerScoreKeeper.Score2); //set the text in the text component
+}
+}
