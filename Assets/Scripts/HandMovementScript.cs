f5f94207c7cde1a855c87a76a6d4a8d240ccde5f
Eric Kim <bosung90@gmail.com>
2014-11-08 16:47:16 -0800

    committing unity plugin for kinect( YOU NEED UNITY PRO!!)

---
+++ Assets/Scripts/HandMovementScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandMovementScript : MonoBehaviour {
+
+	private GameObject RightHand; // I create variable
+	void Start () {
+	}
+	void Update ()
+	{
+		// If Right Hand is empty null it finding else it changing hand position
+		if( RightHand == null)
+		{
+			RightHand = GameObject.Find("HandRight");
+		}
+		else
+		{
+			gameObject.transform.position = new Vector3( -RightHand.transform.position.x,
+			                                            RightHand.transform.position.y,
+			                                            transform.position.z
+			                                            );
+		}
+	}
+}
