f1077102bee595cf9f013d1c881b60039f0dc7ce
Michael Hiland <g2c9@ugrad.cs.ubc.ca>
2014-11-09 08:25:08 -0800

    score bar

---
+++ Assets/KinectView/Scripts/PlayerScoreKeeper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class PlayerScoreKeeper {
+
+	public static float Health1 = 1f;
+	public static float Health2 = 1f;
+
+
+}
---
+++ Assets/ScoreBarPlayer1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreBarPlayer1 : MonoBehaviour {
+	private Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		image.fillAmount = PlayerScoreKeeper.Health1;
+	}
+}
